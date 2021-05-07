ee5f1408e3dceca74114e814596d963ce8def0c7
amy_p <ampreci@gmail.com>
2021-05-07 16:44:47 -0700

    Started business page and fixed video rotation links

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ if ( mediaQuery.matches ) {
     document.getElementById("checkbox").checked = true;
     }
 }
+
+
+function playVideo(sourceId, targetId) {    
+    if (typeof(sourceId)=='string') {
+        sourceId=document.getElementById(sourceId);
+    }    
+    
+    if (typeof(targetId)=='string') {
+        targetId=document.getElementById(targetId);
+    }    
+    
+    targetId.innerHTML=sourceId.innerHTML;    
+    return false; 
+}
